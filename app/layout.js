71210dd47a8f34fb455024f9ab8ae37f6ca69af3
ferchoeth <ferchoeth@gmail.com>
2023-08-12 17:58:20 -0600

    setup dev env

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+//import "@/styles/globals.css";
+import { siteConfig } from "@/config/site";
+
+export const metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon-16x16.png",
+		apple: "/apple-touch-icon.png",
+	},
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<head />
+			<body>
+        {children}
+			</body>
+		</html>
+	);
+}
