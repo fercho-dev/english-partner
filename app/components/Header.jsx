15bf9be9b4e816555d70ea56086e636ab4066155
santiagocamposenr <santiago.campos.enriquez@gmail.com>
2023-09-05 22:06:25 -0600

    Add right features in the navbar and move them to the left

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,20 +1,22 @@
 'use client'
 
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Image} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem,
+        Link, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem,
+        Image} from "@nextui-org/react";
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Help & Feedback",
-    "Log Out",
+    "Chat",
+    "Short Story",
+    "Vocabulary",
+    "Take a test",
   ];
 
   return (
-    <Navbar className="bg-primary text-white" onMenuOpenChange={setIsMenuOpen}>
+    <Navbar className="bg-primary text-white justify-start" onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
@@ -32,40 +34,32 @@ export function Header() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-20">
         <NavbarItem>
           <Link className="text-white" color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link className="text-white" href="#" aria-current="page">
-            Customers
+            Chat
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link className="text-white" color="foreground" href="#">
-            Integrations
+            Short Story
           </Link>
         </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link className="text-white" href="#">Login</Link>
+        <NavbarItem>
+          <Link className="text-white" color="foreground" href="#">
+            Vocabulary
+          </Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} className="text-white" color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <Link className="text-white" color="foreground" href="#">
+            Take a test
+          </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
               className="w-full"
               href="#"
               size="lg"
