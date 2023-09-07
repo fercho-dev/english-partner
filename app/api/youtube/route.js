86929cb11c4110c54a103a0b5f80b7c9baa66390
ferchoeth <ferchoeth@gmail.com>
2023-09-06 18:28:06 -0600

    add duration parameter, default values and no keyword error

--- app/api/youtube/route.js
+++ app/api/youtube/route.js
@@ -4,12 +4,17 @@ const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const baseURL = 'https://www.googleapis.com/youtube/v3/search';
 
 export async function GET(request) {
-  const keyword = request.nextUrl.searchParams.get('keyword');
-  const maxResults = request.nextUrl.searchParams.get('maxResults');
-  const relevanceLanguage = request.nextUrl.searchParams.get('relevanceLanguage');
+  const keyword = request.nextUrl.searchParams.get('keyword') || null;
+  const maxResults = request.nextUrl.searchParams.get('maxResults') || 3;
+  const relevanceLanguage = request.nextUrl.searchParams.get('relevanceLanguage') || 'es';
+  const videoDuration = request.nextUrl.searchParams.get('videoDuration') || 'short';
 
   try {
-    const response = await fetch(`${baseURL}?part=snippet&maxResults=${maxResults}&q=${encodeURIComponent(keyword)}&type=video&relevanceLanguage=${relevanceLanguage}&videoEmbeddable=true&key=${YOUTUBE_API_KEY}`);
+    if (!keyword) {
+        throw new Error('Missing keyword parameter');
+    }
+
+    const response = await fetch(`${baseURL}?part=snippet&maxResults=${maxResults}&q=${encodeURIComponent(keyword)}&type=video&relevanceLanguage=${relevanceLanguage}&videoEmbeddable=true&videoDuration=${videoDuration}&key=${YOUTUBE_API_KEY}`);
 
     if (!response.ok) {
         throw new Error(`YouTube API responded with ${response.statusText}`);
