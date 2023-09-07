1eb0a30133fc6eee8dc0e35b820095d69bdd640b
santiagocamposenr <santiago.campos.enriquez@gmail.com>
2023-09-07 16:52:12 -0600

    Add Footer Component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer className="bg-primary text-white flex justify-center pt-10 pb-10">
+          <p>Made with 🤍 in Ciudad de Mexico </p>
+    </footer>
+  );
+}
